59cc39b2ff667bbbf59c2ffa8f498e804bea78fb
Iram Hameed <160424449+se-iram@users.noreply.github.com>
2025-07-25 23:55:28 +0500

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -1,1 +1,32 @@
+function startCamera() {
+    fetch('/start_camera', { method: 'POST' })
+        .then(() => {
+            document.getElementById('video-box').style.display = 'block';
+            document.getElementById('start-btn').style.display = 'none';
+            document.querySelector('#webcam-stream').src = "/video_feed";
+        });
+}
 
+function stopCamera() {
+    fetch('/stop_camera', { method: 'POST' })
+        .then(() => {
+            document.getElementById('video-box').style.display = 'none';
+            document.getElementById('start-btn').style.display = 'inline-block';
+            document.querySelector('#webcam-stream').src = "";
+            document.getElementById('result-text').innerText = '';
+        });
+}
+
+function verifyFace() {
+    fetch('/verify', {
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('result-text').innerText = data.result;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('result-text').innerText = 'Error verifying face.';
+    });
+}
